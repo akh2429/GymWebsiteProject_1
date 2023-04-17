7b5d40618fe644d79ac50fd7831120760e63e746
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-17 17:52:26 +0530

    done

--- project_1/src/Pages/HomePage/Home.jsx
+++ project_1/src/Pages/HomePage/Home.jsx
@@ -1,9 +1,18 @@
+import myimg from "./fitness-girl-doing-workout-5k-w6.jpg";
+import "./Home.css";
 import React from "react";
 
 function Home() {
   return (
-    <div>
-      <img  />
+    <div className="imageContainer">
+      <img src={myimg} alt="Image not available" className="myImage" />
+      <div className="imageText">
+        <h1>
+          Take Care of your <span style={{ color: "red" }}>body.</span>
+          <br /> Its the only place you <br />
+          have to <span style={{ color: "red" }}>live.</span>
+        </h1>
+      </div>
     </div>
   );
 }
