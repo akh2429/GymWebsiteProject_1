a3dae25734f114aaf4f61370e2fa470b3e733485
Akhilesh <akhileshsharma2429@gmail.com>
2023-04-18 13:57:15 +0530

    done

--- project_1/src/Pages/HomePage/Home.jsx
+++ project_1/src/Pages/HomePage/Home.jsx
@@ -1,11 +1,26 @@
-import myimg from "./fitness-girl-doing-workout-5k-w6.jpg";
+import { useEffect, useState } from "react";
 import "./Home.css";
-import React from "react";
+import myimg1 from "./img1.jpg";
+import myimg2 from "./img2.jpeg";
+import myimg3 from "./img3.jpg";
+const data = [myimg1, myimg2, myimg3];
 
 function Home() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((index + 1) % data.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [index]);
+
+  console.log(index);
+
   return (
     <div className="imageContainer">
-      <img src={myimg} alt="Image not available" className="myImage" />
+      <img src={data[index]} alt="Image not available" className="myImage" />
       <div className="imageText">
         <h1>
           Take Care of your <span style={{ color: "red" }}>body.</span>
