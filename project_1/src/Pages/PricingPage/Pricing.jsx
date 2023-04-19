b125a8b694b098e54f3ca3d2b8e0c6287df6a07d
Ankur <armanankur@gmail.com>
2023-04-19 22:04:03 +0530

    "pricingModification"

--- project_1/src/Pages/PricingPage/Pricing.jsx
+++ project_1/src/Pages/PricingPage/Pricing.jsx
@@ -1,57 +1,29 @@
-import React from 'react'
-import style from "./Pricing.module.css"
-const Pricing = () => {
-  return (
-    <div className={style.bodyContainer}>
-      <div>
-        <h1 className={style.h1}>Get a exclusive pricing plan</h1>
-      </div>
-      <div className={style.pricingSheetContainer}>
-    <div className={style.mainContainer1}>
-      <div className={style.container1_1}>
-    <h2>Besic</h2>
-    <p className={style.price}>$<span>49</span>/pm</p>
-      </div>
-      <div className={style.container2}>
-    <p>Personal trainer</p>
-    <p>Body building</p>
-    <p>Yoga</p>
-    <p>Cycling</p>
-    <p>Weight loss</p>
-    <button className={style.btn1}>GET STARTED</button>
-      </div>
-    </div>
-     <div className={style.mainContainer2}>
-     <div className={style.container1_2}>
-   <h2>Standard</h2>
-   <p className={style.price}>$<span>79</span>/pm</p>
-     </div>
-     <div className={style.container2}>
-   <p>Personal trainer</p>
-   <p>Body building</p>
-   <p>Yoga</p>
-   <p>Cycling</p>
-   <p>Weight loss</p>
-   <button className={style.btn2}>GET STARTED</button>
-     </div>
-   </div>
-    <div className={style.mainContainer3}>
-    <div className={style.container1_3}>
-  <h2>Enterprice</h2>
-  <p className={style.price}>$<span>109</span>/pm</p>
-    </div>
-    <div className={style.container2}>
-  <p>Personal trainer</p>
-  <p>Body building</p>
-  <p>Yoga</p>
-  <p>Cycling</p>
-  <p>Weight loss</p>
-  <button className={style.btn3}>GET STARTED</button>
-    </div>
-  </div>
-  </div>
-  </div>
-  )
+import React from "react"
+import styled from "./Pricing.module.css"
+import Card from "./Card"
+import cardData from "./CardData"
+
+const ncard=(elem) => {
+ return (
+  <>
+  <Card  subscription={elem.subscription} amount ={elem.amount} facilities={elem.facilities} />
+  </>
+ )
 }
 
-export default Pricing;
+const Pricing=()=> {
+
+  return(
+ <div className={styled.mainContainer}>
+
+    <div className={styled.cards}>
+
+    {cardData.map(ncard)}
+    </div>
+    
+   </div>
+
+    
+  )
+}
+export default Pricing
